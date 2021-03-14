61d3827caf3bfc6c03e76f14463360e05f9dfb25
yao-peijun <peijun_yao@hotmail.com>
2021-03-14 11:28:42 +0800

    added router and switch to display between movies and movie details in homepage. Moved movies display as new component, and imported as component

--- src/components/home/HomeMain.jsx
+++ src/components/home/HomeMain.jsx
@@ -1,74 +1,46 @@
-import React, { useState, useEffect } from "react";
-import { Grid, Card, CardActionArea, CardMedia } from "@material-ui/core";
-import axios from "axios";
+import React, { useState } from "react";
+import { withRouter, Switch, Route, useRouteMatch } from "react-router-dom";
 
-//  helpers
+// helpers
 import useGlobalStyles from "../../style/GlobalStyles";
-import Config from "../../config/config";
 
 // components
-import Loading from "../common/Loading";
-import LoadingError from "../common/LoadingError";
+import MoviewsDisplayMain from "../movies-display/MoviesDisplayMain";
+import MovieDetailsMain from "../movie-details/MovieDetailsMain";
+import Error from "../common/Error";
 
 const HomeMain = () => {
   // styles
   const globalClasses = useGlobalStyles();
 
   // data
-  const config = Config();
+  const { path } = useRouteMatch();
 
   // states
-  const [movieData, setMovieData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState({});
 
-  // get movie data
-  useEffect(() => {
-    if (movieData.length === 0) {
-      axios
-        .get(config.APIs.getMovieData)
-        .then((res) => {
-          setMovieData(res.data);
-          setLoading(false);
-        })
-        .catch(() => {
-          setError(true);
-          setLoading(false);
-        });
-    }
-  });
-
-  // movie display cards
-  const img = require.context("../../asset/image/main-page", true);
-  const movieDisply = movieData.map((movie, index) => {
-    const key = `${movie.genre}_${index}`;
-    return (
-      <Grid item xs={6} sm={6} md={4} lg={4} xl={4} key={key}>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              style={{ height: "220px", paddingTop: "2%" }}
-              image={img(`./${movie.image}`).default}
-              title={movie.name}
-            />
-          </CardActionArea>
-        </Card>
-      </Grid>
-    );
-  });
-
-  let mainDisplay = movieDisply;
-  if (loading) mainDisplay = <Loading />;
-  else if (error) mainDisplay = <LoadingError />;
+  // functions
+  const handleViewMovieDetails = (selectedMovieData) => {
+    setSelectedMovie(selectedMovieData);
+  };
 
   // render
   return (
     <div className={globalClasses.contentRoot}>
-      <Grid container spacing={4} className={globalClasses.fullHeight}>
-        {mainDisplay}
-      </Grid>
+      <Switch>
+        {/* movies display */}
+        <Route exact path={path}>
+          <MoviewsDisplayMain handleViewMovieDetails={handleViewMovieDetails} />
+        </Route>
+
+        {/* display movie details */}
+        <Route path={`${path}/:id`}>
+          <MovieDetailsMain selectedMovie={selectedMovie} />
+        </Route>
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 };
 
-export default HomeMain;
+export default withRouter(HomeMain);
