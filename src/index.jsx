38b2f53a8fb0de627c42b6ab52dc07d0038f5c3b
yao-peijun <peijun_yao@hotmail.com>
2021-03-14 17:14:27 +0800

    added StoreProvider to use useContext

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { StoreProvider } from "./context/Store";
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </HashRouter>,
   document.getElementById("root")
 );
